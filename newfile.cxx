0829791e877c45bf9c91b0f79f813955260a5420
mayanksharma007ai <ms20061114@gmail.com>
2025-04-15 17:49:33 +0530

    Add files via upload

---
+++ newfile.cxx
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{ int a,b;// declare the variables< 
+	printf("enter no. a \n");
+	scanf("%d",&a);
+	
+	printf("enter no. b\n");
+	scanf("%d",&b);
+	
+	printf("the sum is %d\n", a+b);
+	 
+	return  0;
+}  
